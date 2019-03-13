a2b5f40734b872889b8eadbc31f0e9829dc5b49f
Robbe De Proft <rdeproft12@gmail.com>
2019-03-13 20:01:24 +0100

    tried adding to database

--- server/restoweb/models.py
+++ server/restoweb/models.py
@@ -3,25 +3,36 @@ from restoweb import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.column(db.String(25), nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
 
-    def __repr__(self):
-        return f"User({self.id}, {self.username})"
+    username = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
 
 
 class Resto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    location = db.Column(db.String(50), nullable=False)
 
-    def __repr__(self):
-        return f"Resto({self.id}, {self.name}, {self.location})"
+    name = db.Column(db.String(50), nullable=False)
+
+    zip_code = db.Column(db.String(10))
+    city = db.Column(db.String(30))
+    address = db.Column(db.String(50))
+
+    campus = db.Column(db.String(50), nullable=True)
+    description = db.Column(db.Text, nullable=True)
+
+
+class Schedules(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    time_open = db.Column(db.Time)
+    working_time = db.Column(db.Integer)
+
+    resto_id = db.Column(db.Integer, db.ForeignKey('resto.id'))
 
 
 class Menu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
 
-    def __repr__(self):
-        return f"Menu({self.date})"
+    date = db.Column(db.Date)
+
+    resto_id = db.Column(db.Integer, db.ForeignKey('resto.id'))
--- server/restoweb/routes.py
+++ server/restoweb/routes.py
@@ -1,7 +1,14 @@
 from restoweb import app
+from restoweb import models
+from flask import render_template
 
 
 @app.route('/')
 def index():
-    return "RestoWeb"
+    return render_template("index.html")
 
+
+@app.route('/restos')
+def show_restos():
+    resto_list = models.Resto.query.all()
+    return render_template("restos.html", resto_list=resto_list)
