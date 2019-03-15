7ca756535375f66f5fa991a247f1b072d972e7c0
abeforgit <arnebertrand@gmail.com>
2019-03-15 15:22:02 +0100

    added commit hook

--- server/restoweb/db_script.py
+++ server/restoweb/db_script.py
@@ -5,31 +5,31 @@ db.drop_all()
 db.create_all()
 
 db.session.add(models.Resto(
-        name="Resto De Brug",
-        zip_code="9000",
-        city="Gent",
-        address="Sint-Pietersnieuwstraat 45",
-        campus="Campus Ufo"
-    )
+    name="Resto De Brug",
+    zip_code="9000",
+    city="Gent",
+    address="Sint-Pietersnieuwstraat 45",
+    campus="Campus Ufo"
+)
 )
 
 db.session.add(models.Resto(
-        name="Resto Campus Sterre",
-        zip_code="9000",
-        city="Gent",
-        address="Krijgslaan 281",
-        campus="Campus Sterre",
-        description="De resto bevindt zich in gebouw S5."
-    )
+    name="Resto Campus Sterre",
+    zip_code="9000",
+    city="Gent",
+    address="Krijgslaan 281",
+    campus="Campus Sterre",
+    description="De resto bevindt zich in gebouw S5."
+)
 )
 
 db.session.add(models.Resto(
-        name="Resto Kantienberg",
-        zip_code="9000",
-        city="Gent",
-        address="Stalhof 45",
-        description="De resto bevindt zich op het gelijkvloers van Home Canterbury."
-    )
+    name="Resto Kantienberg",
+    zip_code="9000",
+    city="Gent",
+    address="Stalhof 45",
+    description="De resto bevindt zich op het gelijkvloers van Home Canterbury."
+)
 )
 
 db.session.commit()
