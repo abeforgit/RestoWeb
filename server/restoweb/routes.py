2f217ed445ccd4e7ee6c9defc4ac476b95fd80f3
Robbe De Proft <rdeproft12@gmail.com>
2019-04-01 15:16:49 +0200

    CRUD template

--- server/restoweb/routes.py
+++ server/restoweb/routes.py
@@ -1,6 +1,7 @@
 from restoweb import app
 from restoweb import models
 from flask import render_template
+from flask import request
 
 
 @app.route('/')
@@ -8,39 +9,50 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/restos')
-def show_restos():
-    resto_list = models.Resto.query.all()
-    return render_template("restos.html", resto_list=resto_list)
+@app.route('/restos', methods=['GET', 'POST'])
+def restos():
+    if request.method == 'GET':
+        resto_list = models.Resto.query.all()
+        return render_template("restos.html", resto_list=resto_list)
 
 
-@app.route('/restos/<int:resto_id>')
-def resto_info(resto_id):
-    # Find resto and schedules matching the given id
-    # If no resto found, return 404
-    resto = models.Resto.query.get_or_404(resto_id)
-    schedules = models.Schedule.query.filter_by(resto_id=resto_id).all()
+@app.route('/restos/<int:resto_id>', methods=['GET', 'DELETE'])
+def restos_info(resto_id):
+    if request.method == 'GET':
+        resto = models.Resto.query.get_or_404(resto_id)
+        schedules = models.Schedule.query.filter_by(resto_id=resto_id).all()
 
-    return render_template("resto_info.html", resto=resto, schedules=schedules)
-
-
-@app.route('/dishes')
-def show_dishes():
-    dish_list = models.Dish.query.all()
-    return render_template("dishes.html")
+        return render_template("restos_info.html", resto=resto, schedules=schedules)
 
 
-@app.route('/dishes/<int:dish_id>')
-def dish_info(dish_id):
-    dish = models.Dish.query.get_or_404(dish_id)
-    return render_template("dish_info.html")
+@app.route('/restos/<int:resto_id>/menus', methods=['GET', 'DELETE'])
+def resto_menus(resto_id):
+    if request.method == 'GET':
+        menus = models.Menu.query.filter_by(resto_id=resto_id)
+
+        return render_template("")
 
 
-@app.route('/login')
-def login():
-    return render_template("login.html")
+@app.route('/menus', methods=['GET', 'POST'])
+def menus():
+    pass
 
 
-@app.route('/register')
-def register():
-    return render_template("register.html")
+@app.route('/menus/<int:menu_id>')
+def menus_info(menu_id):
+    pass
+
+
+@app.route('/menus/<int:menu_id>/dishes', methods=['GET', 'POST'])
+def menus_dishes(menu_id):
+    pass
+
+
+@app.route('/dishes', methods=['GET', 'POST'])
+def dishes():
+    pass
+
+
+@app.route('/dishes/<int:dish_id>', methods=['GET', 'DELETE', 'PUT'])
+def dishes_info(dish_id):
+    pass
