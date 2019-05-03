2ad56d58e5bc47c443c683acec415962ad7ce37c
redfast00 <redfast00@gmail.com>
2019-05-03 14:20:21 +0200

    Added admin column, ran flask db migrations

--- server/restoweb/models.py
+++ server/restoweb/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
 
     username = db.Column(db.String(50), nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
 
 
 class Resto(db.Model):
