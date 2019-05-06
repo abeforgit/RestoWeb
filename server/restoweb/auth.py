abd852e52a49bd14031f73ea2d0a43a6b8eb53ab
abeforgit <arnebertrand@gmail.com>
2019-05-07 01:26:17 +0200

    removed redundant if checks

--- server/restoweb/auth.py
+++ server/restoweb/auth.py
@@ -18,31 +18,10 @@ def load_user_from_request(r):
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        if not request.json:
-            return Response(status=400)
+    if not request.json:
+        return Response(status=400)
 
-        if not current_user.is_authenticated:
-            username = request.json.get('username')
-            password = request.json.get('password')
-
-            if not username:
-                return Response(status=401)
-            if not password:
-                return Response(status=401)
-            user = User.query.filter_by(username=username).first()
-            if not user or not bcrypt.check_password_hash(user.password_hash, password):
-                return Response(status=401)
-            login_user(user, remember=True)
-
-        return jsonify({"token": current_user.apikey})
-
-
-@app.route('/signup', methods=['POST'])
-def signup():
-    if request.method == 'POST':
-        if (not request.json):
-            return Response(status=400)
+    if not current_user.is_authenticated:
         username = request.json.get('username')
         password = request.json.get('password')
 
@@ -51,19 +30,37 @@ def signup():
         if not password:
             return Response(status=401)
         user = User.query.filter_by(username=username).first()
-        if user:
-            return Response(status=409)
+        if not user or not bcrypt.check_password_hash(user.password_hash, password):
+            return Response(status=401)
+        login_user(user, remember=True)
 
-        password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-        new_user = User(username=username, password_hash=password_hash)
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user)
-        return jsonify({"token": new_user.apikey}), 201
+    return jsonify({"token": current_user.apikey})
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    if (not request.json):
+        return Response(status=400)
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    if not username:
+        return Response(status=401)
+    if not password:
+        return Response(status=401)
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return Response(status=409)
+
+    password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username=username, password_hash=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    login_user(new_user)
+    return jsonify({"token": new_user.apikey}), 201
 
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    if request.method == 'POST':
-        logout_user()
-        return Response(status=200)
+    logout_user()
+    return Response(status=200)
